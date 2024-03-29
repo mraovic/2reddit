8032ae8cf8560f99a4cd56c9569f465ba1fbb307
mraovic <mraovic111@gmail.com>
2024-03-29 13:25:02 +0100

    user fix

---
+++ src/user/user.entity.ts
@@ -0,0 +1,19 @@
+import {Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
+
+@Entity('users')
+export class User{
+    @PrimaryGeneratedColumn()
+    id:number;
+    @Column()
+    first_name: string;
+    @Column()
+    last_name: string;
+    @Column()
+    email: string;
+    @Column()
+    pass: string;
+    @CreateDateColumn()
+    created_at: Date;
+    @UpdateDateColumn()
+    updated_at: Date;
+}
